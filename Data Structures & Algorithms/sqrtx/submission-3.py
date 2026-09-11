3f5431210f0f02ba8fb8b0ddf52f89cc2468a7e4
paulantony066 <paulantony066@gmail.com>
2026-09-11 09:46:27 +0530

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        
+        ans=0
+        
+        l=0
+        r=x
+
+        while l<=r:
+            mid=(l+r)//2
+
+            if mid*mid>x:
+                r=mid-1
+            elif mid*mid<=x:
+                ans=mid
+                l=mid+1
+        
+
+        return ans
+
+
